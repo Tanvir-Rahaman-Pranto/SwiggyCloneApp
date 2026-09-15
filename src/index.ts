cce8797aadcf4dc5866b71652312cac9180e9bc8
Prantos laptop <tanvir71648@gmail.com>
2026-09-15 18:38:47 +0600

    Learned the promise & syntax of it and tried it

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,45 @@ let app: express.Application = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+
+function xyz(){
+    return true;
+}
+
+function abc(){
+    return false;
+}
+
+function result():Promise<string>{
+    return new Promise((resolve, reject) => {
+        if(xyz()){
+            resolve('xyz is true');
+        }else if (abc()){
+            resolve('abc is true');
+        }else{
+            reject('Neither xyz nor abc is true');
+        }
+    });
+}
+/*
+result().then((result)=>{
+    console.log(result);
+}).catch((error)=>{
+    console.log(error);
+});*/
+
+async function final(){
+    try{
+        const resultValue = await result();
+        return resultValue;
+    }catch(error){
+        return error;
+    }
+}
+
+final().then((result)=>{
+    console.log(result);
+}).catch((error)=>{
+    console.log(error);
+});
